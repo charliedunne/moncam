5b3df1e1466ff320be386c77a487d40c735d6c30
charlie.dunne <charlie.dunne@gmail.com>
2018-07-01 08:17:58 +0000

    Perform a proper securty management of passwords using md5 hashes

--- webapp/app.py
+++ webapp/app.py
@@ -1,15 +1,31 @@
 from flask import Flask, render_template, session, request, flash
-from picamera import PiCamera
+from picamera import PiCamera, Color
 from time import sleep, gmtime, strftime
 from dateutil import tz
 import datetime
 import time
 import os
+import hashlib
+import pickle
 
 app = Flask(__name__)
 
 # Path of the images
 IMG_PATH="/home/pi/moncam/webapp/static"
+USER_PASS='/home/pi/moncam/webapp/private/pass_user'
+ROOT_PASS='/home/pi/moncam/webapp/private/pass_root'
+
+
+def get_timestamp():
+    
+    # Timestamp in Local Area
+    from_zone = tz.gettz('UTC')
+    to_zone = tz.gettz('Europe/Madrid')
+    utc = time.gmtime()
+    timestamp = datetime.datetime.now(tz.gettz("Europe/Madrid")).isoformat()
+
+    return timestamp
+    
 
 @app.route('/')
 def index():
@@ -21,19 +37,34 @@ def index():
         # Take image
         with PiCamera() as camera:
 
+            # Timestamp
+            timestamp = get_timestamp()
+
             # Camera configuration parameters
             camera.resolution = (3280, 2464)
             camera.rotation = -90
             camera.shutter_speed = 0 # AUTO
-            camera.exposure_mode = 'night'
-            sleep(1);
+            camera.annotate_text = timestamp
+            camera.annotate_background = Color('white')
+            camera.annotate_foreground = Color('black')
+            camera.annotate_text_size = 50
+            camera.iso = 0
+            #camera.drc_strength = 'high'
+            camera.brightness = 50
+            #camera.contrast = 10
+            camera.exposure_mode = 'auto'
+            camera.sensor_mode = 2
+            camera.framerate = 1
+            camera.meter_mode = 'matrix'
+            #camera.sharpness = -50
+            camera.still_stats = True
+            #camera.awb_mode = 'sunlight'
+            camera.start_preview()
+            sleep(2);
 
-            from_zone = tz.gettz('UTC')
-            to_zone = tz.gettz('Europe/Madrid')
-            utc = time.gmtime()
-            timestamp = datetime.datetime.now(tz.gettz("Europe/Madrid")).isoformat()
             #timestamp = strftime("%Y-%m-%d %H:%M:%S", local_timestamp)
-            camera.capture(os.path.join(IMG_PATH, 'now.jpg'))
+            camera.capture(os.path.join(IMG_PATH, 'now.jpg'), quality=100, format="jpeg")
+            camera.stop_preview()
 
             return render_template('index.html', image='now.jpg', timestamp=timestamp)
 
@@ -44,11 +75,31 @@ def maxsnapshoot(image):
     if not session.get('logged_in'):
         return render_template('login.html')
     else:
-        return render_template('maxsnapshoot.html', image=image)
+        
+        timestamp = get_timestamp()
+
+        return render_template('maxsnapshoot.html', image=image, timestamp=timestamp)
 
 @app.route('/login', methods=['POST'])
 def authenticate():
-    if request.form['password'] == 'admin':
+
+    # Open the user hash
+    f_in = open(USER_PASS, 'rb')
+    user_hash = pickle.load(f_in)
+    f_in.close()
+
+    # Open the root hash
+    f_in = open(ROOT_PASS, 'rb')
+    root_hash = pickle.load(f_in)
+    f_in.close()
+
+    # Obtain the pasword
+    clear_pwd = request.form['password']
+
+    # MD5 the password
+    md5_pwd = hashlib.md5(clear_pwd.encode('utf8')).hexdigest()
+
+    if (md5_pwd == user_hash) or (md5_pwd == root_hash):
         session['logged_in'] = True
     else:
         flash('wrong password')
@@ -65,6 +116,6 @@ if __name__ == '__main__':
 
     app.secret_key = os.urandom(12)
     # Run the Web Server
-    app.run(debug=False, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5000)
 
     
